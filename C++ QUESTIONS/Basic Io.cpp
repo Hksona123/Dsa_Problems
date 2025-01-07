9d88695cdbc72b619d917ba8c523edd5670f5d74
Harsh Kumar Sona <134693325+Hksona123@users.noreply.github.com>
2025-01-07 21:24:38 +0530

    It contain the basics of STL.

---
+++ C++ QUESTIONS/Basic Io.cpp
@@ -0,0 +1,37 @@
+//#include<iostream>//It include only some library
+#include<bits/stdc++.h>//It include all the libraries on c++.
+using namespace std;
+int main(){
+//	OUTPUT
+//	cout<< "Hey string"<< endl<< "Hey Raj";
+//	cout<< "Hey string"<< "\n";
+//	cout<< "Hey string";
+
+//	INPUT
+//	int x,y;
+//	cin>>x>>y;
+//	cout<< "value of x"<<x <<"value of y"<<y;
+
+//String and getline
+//string str;
+//getline(cin,str);
+//cout<<str;
+
+//We can use if and else-if only in c++
+
+
+//Functions:- They are set of code which performs searching for you. They are used to modularise code.
+//They are used to increase readability, used to use same code multiple times. 
+//void-> which does not returns anything
+//Array always take value by reference in function.
+
+//Time complexity:- The rate at which time taken increases with respect to the input size.
+//Always measure it in worst case scenario, avoid constants, avoid lower values.
+
+//Space Complexity:- Auxillary space(Space that you take to solve the problem) + Input Space(The space that you take to share the input).
+/////////////////////////(In interview, never do anything to the input) 
+return 0;
+
+
+}
+
