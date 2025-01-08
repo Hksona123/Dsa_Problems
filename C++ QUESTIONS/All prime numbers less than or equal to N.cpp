ed892c2d3c4aae656aa27a545e82de86039bdf0c
Cloud_DevUser <134693325+Hksona123@users.noreply.github.com>
2025-01-08 14:40:35 +0530

    Add files via upload

---
+++ C++ QUESTIONS/All prime numbers less than or equal to N.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define FORk(i,a,b,k) for(int i=(a);i<(b);i+=k)
+#define RFOR(i,a,b) for(int i=(a);i>=(b);i--)
+#define RFORk(i,a,b,k) for(int i=(a);i>=(b);i-=k)
+
+const int N=10000000;
+bool pr[N+1];
+
+void sieve()
+{
+	FOR(i,2,N+1) pr[i]=true;
+	FOR(i,2,N+1) 
+	{
+		
+	}
+}
+void solve()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdiio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ C++ QUESTIONS/Template for c++.cpp
@@ -0,0 +1,25 @@
+# cook your dish here
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define FORk(i,a,b,k) for(int i=(a);i<(b);i+=k)
+#define RFOR(i,a,b) for(int i=(a);i>=(b);i--)
+#define RFORk(i,a,b,k) for(int i=(a);i>=(b);i-=k)
+void solve()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdiio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
